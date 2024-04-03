ab7751068a0c48eceaaa1144688f68b5029a9a96
eveakoko <you@example.com>
2024-04-03 11:39:06 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const form=document.querySelector("#create-task-form")
+ const tasks=document.querySelector("#tasks")
+ console.log(tasks)
+ form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const taskDescription=document.querySelector("#new-task-description")
+  if(taskDescription.value==""){
+    console.log(taskDescription.value)
+    alert("pleas write")
+  }
+  else{
+    const taskList=document.createElement('li');
+    const deleteButton=document.createElement(`button`)
+    taskList.textContent=taskvalue
+    deleteButton.textContent="x"
+    taskDescription.appendChild(taskList)
+    taskList.appendChild(deleteButton)
+    deleteButton.addEventListener('click',()=>{
+      taskList.remove()
+    })
+    }
+  }
+ )
+
 });
